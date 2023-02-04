dab331d25c7a742a5c3d11a742f96d840eb7e38a
Gabriele Bilello <gabriele.bilello1@gmail.com>
2023-02-04 10:04:15 +0100

    added sub-components

--- src/app/components/grid/grid.module.ts
+++ src/app/components/grid/grid.module.ts
@@ -4,14 +4,16 @@ import { GridComponent } from './grid.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SearchPipe } from './pipes/search.pipe';
 import { SortPipe } from './pipes/sort.pipe';
-
-
+import { GridHeaderComponent } from './components/grid-header.component';
+import { GridBodyComponent } from './components/grid-body.component';
 
 @NgModule({
   declarations: [
     GridComponent,
     SearchPipe,
-    SortPipe
+    SortPipe,
+    GridHeaderComponent,
+    GridBodyComponent,
   ],
   exports: [
     GridComponent
